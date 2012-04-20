5276fc4e216a5d9547868c1d1cb3f8096a79cb19
Buti <buti@nobuti.com>
2012-04-20 13:27:14 +0200

    Fix trigger on cached events to avoid unbind overlap. Several changes in style to better perfomance.

--- observer.js
+++ observer.js
@@ -1,26 +1,26 @@
 var _m = function(window, document, undefined){
 	
-	var space = /\s+/,
-  		_events = [];
-	
 	var _mod = {},
+		space = /\s+/,
+  		_events = [],
 		slice = Array.prototype.slice;
 	
 	_mod.bind = function(event, fn){
 		if ( !event ) return;
 		var parts = event.split(space),
 			l = parts.length;
-		for (var i = 0; i < l; i++) {
-			_events[parts[i]] = _events[parts[i]] || [];
-			_events[parts[i]].push(fn);
+			console.log(l);
+		while ( l-- ) {
+			console.log(parts[l]);
+			_events[parts[l]] = _events[parts[l]] || [];
+			_events[parts[l]].push(fn);
 		};
 	}
 	_mod.unbind = function(event, fn){
 		if ( !event ) return;
 		var parts = event.split(space),	l = parts.length, eventName;
-		for (var i = 0; i < l; i++) {
-			if ( parts[i] in _events ){
-				eventName = parts[i];
+		while (l--) {
+			if (( eventName = parts[l] ) in _events ){
         		_events[eventName].splice(_events[eventName].indexOf(fn), 1);
 			}
 		};
@@ -28,27 +28,31 @@ var _m = function(window, document, undefined){
 	_mod.unbindAll = function(event){
 		if ( !event ) return;
 		var parts = event.split(space),	l = parts.length, eventName;
-		for (var i = 0; i < l; i++) {
-			if ( parts[i] in _events ){
-				eventName = parts[i];
+		while ( l-- ) {
+			if (( eventName = parts[l] ) in _events ){
         		delete _events[eventName];
 			}
 		};
 	}
 	_mod.trigger = function(event){
 		if ( !event ) return;
-		var parts = event.split(space),	l = parts.length, arr, k,
-		// arguments[0] === event
-		args = slice.call(arguments,1);
-		for (var i = 0; i < l; i++) {
-			if ( parts[i] in _events ){
-				arr = _events[parts[i]];
+		var parts = event.split(space), _e = _mod.mix(_events),	l = parts.length, arr, k, eventName, args = slice.call(arguments,1);
+		while ( l-- ){
+			if (( eventName = parts[l] ) in _e ){
+				arr = _e[eventName];
 				k = arr.length || 0;
-				for (var j = 0; j < k; j++) {
-					arr[j].apply(this,args);
+				while ( k-- ) {
+					arr[k].apply(this,args);
 				};
 			}
 		};
+	}
+	_mod.mix = function(object) {
+		var o = {};
+		for (var i in object){
+			o[i] = object[i];
+		}
+		return o;
 	}
 
 }(this, document)
